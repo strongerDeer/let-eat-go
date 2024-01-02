5f1fe61ecf2c579dd8684af5d2f7c06bd9ca2118
strongerDeer <stronger_deer@naver.com>
2024-01-02 20:48:42 +0900

    📝 Add: 페이지 생성

---
+++ src/pages/stores/[id]/edit.tsx
@@ -0,0 +1,7 @@
+import { useRouter } from 'next/router';
+
+export default function StoreEditPage() {
+  const router = useRouter();
+  const { id } = router.query;
+  return <>StoreEditPage:{id}</>;
+}
---
+++ src/pages/stores/[id]/index.tsx
@@ -0,0 +1,7 @@
+import { useRouter } from 'next/router';
+
+export default function StoreDetailPage() {
+  const router = useRouter();
+  const { id } = router.query;
+  return <>StoreDetailPage:{id}</>;
+}

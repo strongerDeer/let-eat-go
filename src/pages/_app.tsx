68df3466bda86a948033546d48c96353acf524f1
strongerDeer <stronger_deer@naver.com>
2024-01-07 16:36:15 +0900

    🌱 Add: react-query 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,18 @@ import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
 
 import Layout from '@/components/layouts';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
